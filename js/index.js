5d59709a07d14f213b215cd3a5da47a1dfbe0b41
Yan <o.loo2016@gmail.com>
2017-12-20 01:31:43 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -4,15 +4,19 @@ document.body.onscroll = function none() {
  var nav = document.querySelector("#scroll-nav");
     var nav_static = document.querySelector("#nav-static");
     if (window.pageYOffset <   document.documentElement.clientHeight + 50) {
-        top.style.display = 'none';
-        //nav.style.display = 'block';
-        nav_static.style.display = 'none';
+        
+        if( top != null)
+            top.style.display = 'none';
+
+        if( nav_static != null)
+            nav_static.style.display = 'none';
     }
     else
     {
-        top.style.display = 'block';
-          //nav.style.display = 'none';
-       nav_static.style.display = 'block';
+        if( top != null)
+            top.style.display = 'block';
+        if( nav_static != null)
+            nav_static.style.display = 'block';
     }
 };
 
